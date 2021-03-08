d997891031e015c89684ea2f4e49e339ece9284c
summaiya14 <summaiyamahmod@gmail.com>
2021-03-08 16:34:59 +0500

    identify people

--- Face_Recognition.py
+++ Face_Recognition.py
@@ -1,10 +1,11 @@
+from PIL import Image
 import face_recognition
 
-#image = face_recognition.load_image_file('./images/group1.jpg')
-#face_locations = face_recognition.face_locations(image)
+image = face_recognition.load_image_file('./images/group1.jpg')
+face_locations = face_recognition.face_locations(image)
 
-#print(face_locations)
-#print(f'There are {len(face_locations)} people in this image')
+print(face_locations)
+print(f'There are {len(face_locations)} people in this image')
 
 img1 = face_recognition.load_image_file('./images/img.jpg')
 img1_encoding = face_recognition.face_encodings(img1)[0]
@@ -20,3 +21,9 @@ if results[0]:
 else: 
     print('Match Not Found')
 
+for face_location in face_locations:
+    top, right, bottom, left = face_location
+
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
---
+++ identify.py
@@ -0,0 +1,53 @@
+import face_recognition
+from PIL import Image, ImageDraw
+
+image_of_bill = face_recognition.load_image_file('./images/summaiya.jpeg')
+bill_face_encoding = face_recognition.face_encodings(image_of_bill)[0]
+
+image_of_steve = face_recognition.load_image_file('./images/faiza.jpeg')
+steve_face_encoding = face_recognition.face_encodings(image_of_steve)[0]
+
+images_face_encodings = [
+  bill_face_encoding,
+  steve_face_encoding,
+]
+
+images_face_names = [
+  "Summaiya Mehmood",
+  "Faiza Qasim",
+]
+
+test_image = face_recognition.load_image_file('./images/summaiya-faiza.jpeg')
+
+face_locations = face_recognition.face_locations(test_image)
+face_encodings = face_recognition.face_encodings(test_image, face_locations)
+
+# Convert to PIL format
+pil_image = Image.fromarray(test_image)
+
+# Create a ImageDraw instance
+draw = ImageDraw.Draw(pil_image)
+
+# Loop through faces in test image
+for(top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+  matches = face_recognition.compare_faces(images_face_encodings, face_encoding)
+
+  name = "Unknown Person"
+
+  # If match
+  if True in matches:
+    first_match_index = matches.index(True)
+    name = images_face_names[first_match_index]
+  
+  # Draw box
+  draw.rectangle(((left, top), (right, bottom)), outline=(0,0,0))
+
+  # Draw label
+  text_width, text_height = draw.textsize(name)
+  draw.rectangle(((left,bottom - text_height - 10), (right, bottom)), fill=(0,0,0), outline=(0,0,0))
+  draw.text((left + 6, bottom - text_height - 5), name, fill=(255,255,255,255))
+
+del draw
+
+# Display image
+pil_image.show()
